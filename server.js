9e65c64ef4df0bfd3dccf5d7591bee4a287f926c
Norman Barber <normanebarber@gmail.com>
2015-01-17 11:52:59 -0500

    recursively read folder returning total count for each file format

--- server.js
+++ server.js
@@ -3,26 +3,26 @@ var walk = require('walkdir');
 var fsext = require('./fsext.js');
 
 var app = express();
-var start;
-var end;
-app.set('port', process.env.PORT || 8000);
-
+var startTime;
+var endTime;
 var readpath = 'path/to/read/folder';
+
+app.set('port', process.env.PORT || 8000);
 
 app.listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
-	start = new Date();
+	startTime = new Date();
 	walker.walkDir(readpath,function(files){
 		fsext(files)()
 			.fin(function(){
 				console.log('\nstep 3 0f 3: returning performance results')
-				end =  new Date();
-				var diff = end - start;
-				var ms = diff;
-				var min = (ms/1000/60) << 0;
-				var sec = (ms/1000) % 60;
-				sec = Math.round(sec * 100) / 100;
-				console.log(min + ' minutes : ' + sec + ' seconds');
+				endTime =  new Date();
+				var totalTime = endTime - startTime;
+				var ms = totalTime;
+				var minutes = (ms/1000/60) << 0;
+				var seconds = ((ms/1000) % 60) * 100;
+				seconds = ~~seconds / 100;
+				console.log(minutes + ' minutes : ' + seconds + ' seconds');
 			})
 			.fail(function(error){
 				console.log('There was an error thrown while walking your folder structure');
@@ -38,13 +38,13 @@ var walker = {
 		walk(readpath, function(file){
 			files.push(file);
 		})
-			.on('error',function(error){
-				console.log('\nThe folder you are trying to read from is either misspelled or does not exist');
-				console.log('This is the path causing the error = ' + error);
-			})
-			.on('end',function(){
-				return callback(files);
-			})
+		.on('error',function(error){
+			console.log('\nThe folder you are trying to read from is either misspelled or does not exist');
+			console.log('This is the path causing the error = ' + error);
+		})
+		.on('end',function(){
+			return callback(files);
+		})
 	}
 }
 
