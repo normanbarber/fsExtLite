e5ec38bbc494a34ca453f5f8650aea4a02029ef9
Norman Barber <normanebarber@gmail.com>
2015-01-17 12:15:42 -0500

    recursively read folder returning total count for each file format

--- fsext.js
+++ fsext.js
@@ -5,13 +5,13 @@ var _ = require('lodash-node');
 
 var fsmodule = {
 	filepaths: [],
-	filedata: [ ],
-	getStats: function(files){
+	filedata: [],
+	getStats: function(){
 		var all = [];
 		var promise = null;
 		var self = this;
-		var files = this.filepaths.splice(0, 30);  // prevents memory allocation error. comment this line to see error when reading C:/ drive or another folder with a lot of files
-		var filedata = [];
+		var files = this.filepaths.splice(0, 20);  // prevents memory allocation error. comment this line to see error when reading C:/ drive or another folder with a lot of files
+//        var files = this.filepaths;  // uncomment this line to see error
 		_.each(files,function(filepath){
 			var fileformat = path.extname(filepath);
 			var year;
@@ -21,16 +21,16 @@ var fsmodule = {
 					if(stats && stats.mtime && fileformat){
 						year = stats.mtime.getFullYear();
 
-						var findtype = _.find(filedata,{format:fileformat});
+						var findtype = _.find(self.filedata,{format:fileformat});
 						if(findtype && year >= 2010){
-							_.remove(filedata,{format:fileformat});
-							filedata.push({format:fileformat, count:findtype.count+1});
+							_.remove(self.filedata,{format:fileformat});
+							self.filedata.push({format:fileformat, count:findtype.count+1});
 						}
-						else if(year >= 2010 || filedata.length <= 0)
-							filedata.push({format:fileformat,count:1})
+						else if(year >= 2010 || self.filedata.length <= 0)
+							self.filedata.push({format:fileformat,count:1})
 
-						return Q(filedata);
 					}
+					return Q(self.filedata);
 				})
 				.fail(function(error){
 					return Q.reject(error);
@@ -42,6 +42,12 @@ var fsmodule = {
 			.then(function(promises) {
 				return Q(_.map(promises, Q.nearer));
 			})
+			.fin(function(){
+				// IMPORTANT : if you comment the splice() to see the memory allocation error you will need to comment this fin too
+				if(self.filepaths.length > 0){
+					return Q(fsmodule.getStats());
+				}
+			})
 	}
 }
 module.exports = function(readfilepaths) {
@@ -50,7 +56,7 @@ module.exports = function(readfilepaths) {
 		console.log('\nstep 2 0f 3: reading file stats. pleast wait...\n')
 		fsmodule.filepaths = readfilepaths;
 
-		return fsmodule.getStats(readfilepaths)
+		return fsmodule.getStats()
 			.fail(function(error) {
 				return Q.reject(error);
 			})
